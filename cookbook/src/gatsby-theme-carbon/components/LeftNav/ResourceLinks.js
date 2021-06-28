68bf53ef6e30fc51b4f1bf97335448a3bb875401
Brian Lavin <brianlavin@ie.ibm.com>
2021-06-28 14:51:30 +0100

    Draft shell of cookbook

---
+++ cookbook/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
+
+const links = [
+  {
+    title: 'Github',
+    href: 'https://github.com/IBM/spm-chatbot',
+  },
+  {
+    title: 'Knowledge Center',
+    href: 'https://www.ibm.com/support/knowledgecenter/SS8S5A_7.0.11/com.ibm.curam.nav.doc/spm_welcome.html',
+  },
+  {
+    title: 'Change Log',
+    href: 'https://github.com/IBM/spm-chatbot/blob/master/CHANGELOG.md',
+  }
+];
+
+// shouldOpenNewTabs: true if outbound links should open in a new tab
+const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
+
+export default CustomResources;
