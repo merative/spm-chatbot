a56ac1941942034458bd53f207f14deeb51b0d4a
jenny cooper <jcooper3@ie.ibm.com>
2022-01-18 20:52:03 +0000

    resolve dependencies

--- cookbook/gatsby-config.js
+++ cookbook/gatsby-config.js
@@ -24,21 +24,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        // Defaults used for gatsbyImageData and StaticImage
-        defaults: {},
-        // Set to false to allow builds to continue on image errors
-        failOnError: true,
-        // deprecated options and their defaults:
-        base64Width: 20,
-        forceBase64Format: `png`, // valid formats: png,jpg,webp
-        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
-        stripMetadata: true,
-        defaultQuality: 50,
-      },
-    },
-    {
       resolve: 'gatsby-theme-carbon',
       options: {
         isSearchEnabled: true,
