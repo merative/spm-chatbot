9b180fb26c9c266e38f105beae83ebbbc01f2f18
Jenny Cooper <jcooper3@merative.com>
2023-02-09 18:21:30 +0000

    Create index.js

---
+++ cookbook/src/gatsby-theme-carbon/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from 'gatsby-theme-carbon/src/components/Header';
+
+const CustomHeader = (props) => (
+  <Header {...props}>
+    <div>Merative&nbsp;<span>SPM Virtual Assistant</span></div>
+  </Header>
+);
+
+export default CustomHeader;
